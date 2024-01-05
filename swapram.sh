891c6a975fb1b736dccb3346a5c9335d06d2f691
apih46 <khafiz532@gmail.com>
2024-01-05 21:40:01 +0800

    Update swapram.sh

--- swapram.sh
+++ swapram.sh
@@ -2,7 +2,6 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition V1.0
-# Auther  : NevermoreSSH
 # (C) Copyright 2022
 # =========================================
 P='\e[0;35m'
@@ -14,8 +13,7 @@ clear
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
 echo -e " \E[0;47;30m                 SWAP RAM                   \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m
-\033[1;37mSwapRAM By NevermoreSSH\033[0m
-\033[1;37mTelegram : https://t.me/todfix667 \033[0m"
+\033[1;37mSwapRAM \033[0m
 echo -e ""
 echo -e "
  [\033[1;36m•1\033[0m]  Add 512MB RAM
